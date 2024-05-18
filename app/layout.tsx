cec237582707164abfe4057e89c6d3cc5f81e6d2
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-05-18 11:55:48 +0900

    🎉 프로젝트 생성

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "GlobalNomad",
+  description: "여행",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
