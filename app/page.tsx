6ac2b6e6ab972c4b8118fb8751b7326c24d7bcf1
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-05-19 17:44:37 +0900

    ♻️ pretendard 폰트 tailwind css 작동하는지 테스트

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="bg-yellow-300 flex justify-center items-center text-3xl">
+    <h1 className="bg-yellow-300 flex justify-center items-center text-3xl font-pretendard">
       홈페이지(tailwind 적용 테스트 )
     </h1>
   );
