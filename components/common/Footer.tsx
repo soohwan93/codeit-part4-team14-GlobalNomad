532b24efaf33e77b04831992e8229da083550066
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-05-23 18:22:50 +0900

    ✅ footer 및 헤더 컴포넌트 임시 작성해 layout 테스트

---
+++ components/common/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" bg-green-950 text-white text-5xl text-center">
+      Footer입니다
+    </div>
+  );
+};
+
+export default Footer;
