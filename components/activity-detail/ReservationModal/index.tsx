915194b4eeefe36fa73b6d3c315041d1d4f36746
Rhajiit <ksd9638@gmail.com>
2024-05-27 14:20:29 +0900

    💄 tablet 사이즈에서 누락된 ui 자리 추가

--- components/activity-detail/ReservationModal/index.tsx
+++ components/activity-detail/ReservationModal/index.tsx
@@ -3,13 +3,19 @@ import React from "react";
 
 const ReservationModal = () => {
   return (
-    <div className="fixed bottom-0 flex w-screen flex-row justify-between bg-white p-4 outline outline-[1px] outline-[#a1a1a1] md:relative md:bottom-0 md:h-[26.9375rem] md:w-[15.6875rem] md:flex-col md:rounded-xl xl:h-[46.625rem] xl:w-[24rem]">
+    <div className="fixed bottom-0 flex w-screen flex-row justify-between bg-white p-4 outline outline-[1px] outline-[#a1a1a1] md:relative md:bottom-0 md:block md:h-[26.9375rem] md:w-[15.6875rem] md:flex-col md:rounded-xl xl:h-[46.625rem] xl:w-[24rem]">
       <div className="grid grid-cols-2 md:grid-cols-1">
-        <span>가격</span>
+        <span>가격 / </span>
         <span>인원수</span>
         <span className="md:row-start-2">날짜</span>
       </div>
+
       <button>예약하기</button>
+
+      <div className="hidden md:flex md:justify-between">
+        <span>총 합계</span>
+        <span>₩ 10000원</span>
+      </div>
     </div>
   );
 };
