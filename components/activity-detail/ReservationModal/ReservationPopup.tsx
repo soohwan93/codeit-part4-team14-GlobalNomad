01afac2413fd30739ffdec7455e981e04276d74b
Rhajiit <ksd9638@gmail.com>
2024-05-27 16:44:28 +0900

    💄 reservationPopup의 태블릿 형태 추가

--- components/activity-detail/ReservationModal/ReservationPopup.tsx
+++ components/activity-detail/ReservationModal/ReservationPopup.tsx
@@ -13,7 +13,7 @@ const ReservationPopup = ({
   children,
 }: ReservationPopupType) => {
   return (
-    <div className="fixed left-0 top-0 h-screen w-screen bg-white p-6 pb-10">
+    <div className="h-37.5rem] fixed left-0 top-0 h-screen w-screen bg-white p-6 pb-10 md:absolute md:w-[30rem] md:rounded-xl md:pb-8  ">
       <header className="mb-8 flex items-center justify-between text-[1.75rem] font-bold leading-[92.857%]">
         {title}
         <button type="button" onClick={() => setState(false)}>
