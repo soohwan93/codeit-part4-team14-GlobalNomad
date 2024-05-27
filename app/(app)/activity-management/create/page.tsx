0ee0092eeed7d14ba8d3b53df07aa1124baa43f9
Rhajiit <ksd9638@gmail.com>
2024-05-27 14:09:54 +0900

    💄 ReservationModal의 기본적인 형태 구성

--- app/(app)/activity-management/create/page.tsx
+++ app/(app)/activity-management/create/page.tsx
@@ -1,9 +1,14 @@
+import ReservationModal from "@/components/activity-detail/ReservationModal";
 import React from "react";
 
 type Props = {};
 
 const page = (props: Props) => {
-  return <div>page</div>;
+  return (
+    <div className="h-screen w-screen">
+      <ReservationModal />
+    </div>
+  );
 };
 
 export default page;
