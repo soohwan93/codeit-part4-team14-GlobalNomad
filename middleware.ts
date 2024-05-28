d4a275080d7d367f0c7466760116bb577568d0a8
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-05-28 16:15:08 +0900

    ♻️ 조건 수정 및 accessToken을 30분으로 설정, httpOnly 속성 추가, matcher 변경

--- middleware.ts
+++ middleware.ts
@@ -13,48 +13,53 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-  // refreshToken을 사용하여 새로운 accessToken을 발급받는 API 호출
-  // 공통 api 작업이 완료되면 BASE_URL은 상수 값으로 대체
-  const refreshTokenResponse = await fetch(
-    `https://sp-globalnomad-api.vercel.app/4-14/auth/tokens`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken?.value}`,
-      },
-    },
-  );
-  if (refreshTokenResponse.ok) {
-    const refreshTokenData = await refreshTokenResponse.json();
-    const newAccessToken = refreshTokenData.accessToken;
+  if (!accessToken && refreshToken) {
+    try {
+      const refreshTokenResponse = await fetch(
+        `https://sp-globalnomad-api.vercel.app/4-14/auth/tokens`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${refreshToken.value}`,
+          },
+        },
+      );
 
-    // 새로운 accessToken을 쿠키에 설정
-    const response = NextResponse.next();
-    response.cookies.set("accessToken", newAccessToken, {
-      path: "/",
-      maxAge: 60 * 60,
-      secure: true,
-      sameSite: "strict",
-    });
+      if (refreshTokenResponse.ok) {
+        const refreshTokenData = await refreshTokenResponse.json();
+        const newAccessToken = refreshTokenData.accessToken;
 
-    if (url.pathname === "/signin" || url.pathname === "/signup")
-      return NextResponse.redirect(new URL("/folder", request.url));
-    // 현재 페이지에 머무름
-    return response;
+        // 새로운 accessToken을 쿠키에 설정
+        const response = NextResponse.next();
+        response.cookies.set("accessToken", newAccessToken, {
+          path: "/",
+          maxAge: 30 * 60,
+          secure: true,
+          httpOnly: true,
+          sameSite: "strict",
+        });
+
+        // 현재 페이지에 머무름
+        return response;
+      } else {
+        // Refresh token이 유효하지 않으면 로그인 페이지로 리다이렉트
+        return NextResponse.redirect(new URL("/signin", request.url));
+      }
+    } catch (error) {
+      console.error("refreshToken 접근 실패: ", error);
+      return NextResponse.redirect(new URL("/signin", request.url));
+    }
   }
 
   // accessToken과 refreshToken이 모두 없는 경우 로그인 페이지로 리다이렉트
-
-  if (url.pathname === "/folder") {
-    if (!accessToken && !refreshToken) {
-      return NextResponse.redirect(new URL("/signin", request.url));
-    }
+  if (!accessToken && !refreshToken) {
+    return NextResponse.redirect(new URL("/signin", request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/folder/:path*", "/shared/:path*", "/signin", "/signup"],
+  matcher: ["/((?!$|activity-detail).*)"],
 };
