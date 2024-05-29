2c844884a4d4341fb00c6a3002a768e8e24d5bd3
Rhajiit <ksd9638@gmail.com>
2024-05-29 15:40:22 +0900

    📝 jsDoc 추가

--- components/common/useDropdownInput.tsx
+++ components/common/useDropdownInput.tsx
@@ -1,15 +1,16 @@
 "use client";
 import React, { useState } from "react";
 
-interface useDropdownInputType {
-  dataArray: string[];
-  defaultValue: string | null;
-  type: string;
-}
-
+/**
+ *
+ * @param {string[]} dataArray 현재 드롭다운에서 보여줄 데이터 목록을 받습니다.
+ * @param {string} defaultValue 수정 등의 경우 현재 드롭다운에서 기본으로 선택할 값을 받습니다. 없을 경우 null로 처리됩니다.
+ * @param {string} type 현재 드롭다운이 어떤 종류인지 보여줄 항목입니다. defaultValue가 null일 경우, 이것을 보여줍니다.
+ * @returns
+ */
 const useDropdownInput = (
   dataArray: string[],
-  defaultValue: string | null,
+  defaultValue: string | null = null,
   type = "카테고리",
 ) => {
   const [selected, setSelected] = useState(defaultValue);
