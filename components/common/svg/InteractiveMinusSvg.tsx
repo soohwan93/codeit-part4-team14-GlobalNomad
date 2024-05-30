5fa5054a9739d6e04d5190a76e90a055cda96564
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-05-30 17:45:05 +0900

    ✨ 반응형 적용되야하는 svg 적용, onClick 받을 수 있게 수정

---
+++ components/common/svg/InteractiveMinusSvg.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import MinusLargeSvg from "./MinusLargeSvg";
+import MinusSmallSvg from "./MinusSmallSvg";
+
+type Props = { onClick: () => void };
+
+const InteractiveMinusSvg = ({ onClick }: Props) => {
+  return (
+    <>
+      <div className="block md:hidden" onClick={onClick}>
+        <MinusSmallSvg />
+      </div>
+      <div className="hidden md:block" onClick={onClick}>
+        <MinusLargeSvg />
+      </div>
+    </>
+  );
+};
+
+export default InteractiveMinusSvg;
---
+++ components/common/svg/InteractivePlusSvg.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PlusLargeSvg from "./PlusLargeSvg";
+import PlusSmallSvg from "./PlusSmallSvg";
+
+type Props = { onClick: () => void };
+
+const InteractivePlusSvg = ({ onClick }: Props) => {
+  return (
+    <>
+      <div className="block md:hidden" onClick={onClick}>
+        <PlusSmallSvg />
+      </div>
+      <div className="hidden md:block" onClick={onClick}>
+        <PlusLargeSvg />
+      </div>
+    </>
+  );
+};
+
+export default InteractivePlusSvg;
