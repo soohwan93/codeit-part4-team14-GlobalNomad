c48df3b76e2251ce10ce1bbf43f939a212844935
Rhajiit <ksd9638@gmail.com>
2024-05-30 13:06:45 +0900

    🐛 일부 prop순서 조정 및 페이지 원상 복구

--- app/(app)/activity-management/page.tsx
+++ app/(app)/activity-management/page.tsx
@@ -1,11 +1,10 @@
-"use client";
-import useDropdownInput from "@/components/common/useDropdownInput";
 import ColorTest from "@/components/test/ColorTest";
 import React from "react";
 
-const Page = () => {
-  const { selected, renderDropdown } = useDropdownInput(["a", "b", "c"], null);
-  return <div className="h-screen">a{renderDropdown()}a</div>;
+type Props = {};
+
+const page = (props: Props) => {
+  return <ColorTest></ColorTest>;
 };
 
-export default Page;
+export default page;
