4dd1e18b7590570f77d31ffaf6c29c7029b8270d
Rhajiit <ksd9638@gmail.com>
2024-05-31 15:17:24 +0900

    💄 일부 스타일 조정

--- components/activity-detail/ReservationModal/ReservationPopup.tsx
+++ components/activity-detail/ReservationModal/ReservationPopup.tsx
@@ -13,7 +13,7 @@ const ReservationPopup = ({
   children,
 }: ReservationPopupType) => {
   return (
-    <div className="h-37.5rem] md:outline-1px fixed left-0 top-0 h-screen w-screen bg-white p-6 pb-10 shadow-sm md:absolute md:h-min md:w-[30rem] md:rounded-xl md:pb-8 md:outline md:outline-[#A4a1aa]">
+    <div className="md:outline-1px fixed right-0 top-0 h-screen w-screen bg-white p-6 pb-10 shadow-sm md:absolute md:h-min md:w-[30rem] md:rounded-xl md:pb-8 md:outline md:outline-[#A4a1aa]">
       <header className="mb-8 flex items-center justify-between text-[1.75rem] font-bold leading-[92.857%]">
         {title}
         <button
