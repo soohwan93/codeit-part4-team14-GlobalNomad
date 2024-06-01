7a9c8bea9f377d08d86bb17ce04ff624a027d9fc
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-06-01 16:23:45 +0900

    ♻️ 콘솔에 값이 남도록 하고 나중에 지우도록 설정

--- app/api/check-cookie/route.ts
+++ app/api/check-cookie/route.ts
@@ -1,14 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-  const cookieName = "accessToken";
+async function handleRequest(request: NextRequest) {
+  const cookieName = "accessToken"; // 확인하고자 하는 쿠키 이름
+  console.log("쿠키 이름:", cookieName);
 
   // 쿠키를 가져옴
-  const cookieValue = req.cookies.get(cookieName);
+  const cookieValue = request.cookies.get(cookieName);
+  console.log("쿠키 값:", cookieValue);
 
   // 쿠키의 존재 여부를 확인
   const cookieExists = cookieValue !== undefined;
+  console.log("쿠키 존재 여부:", cookieExists);
 
   // JSON 응답으로 쿠키 존재 여부를 반환
-  return NextResponse.json({ cookieExists });
+  return NextResponse.json({ cookieExists }, { status: 200 });
+}
+
+export async function GET(request: NextRequest) {
+  return handleRequest(request);
 }
