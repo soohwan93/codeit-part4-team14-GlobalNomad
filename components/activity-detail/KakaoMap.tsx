c79a5b0f8b9a1e89fa482eef6eb08d1e70b2d723
Rhajiit <ksd9638@gmail.com>
2024-06-02 14:36:06 +0900

    🚧 kakaoMap 컴포넌트 기본 골자 구성

---
+++ components/activity-detail/KakaoMap.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type Props = {};
+
+const KakaoMap = (props: Props) => {
+  return (
+    <div>
+      <div className="h-96 w-96 outline-[1px] outline-black">
+        카카오 맵 들어갈 자리
+      </div>
+      <span>현재 체험의 주소 들어갈 자리</span>
+    </div>
+  );
+};
+
+export default KakaoMap;
