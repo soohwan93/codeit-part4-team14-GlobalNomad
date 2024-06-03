0e302dabd1956ed0cad7c6f81e1593ffba012fe7
Rhajiit <ksd9638@gmail.com>
2024-06-03 16:54:58 +0900

    🤡 ActivityList의 간단한 목데이터 생성

---
+++ components/common/ACTIVITY_LIST.ts
@@ -0,0 +1,60 @@
+interface ActivityManagementCardProps {
+  id: number;
+  userId: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  address: string;
+  bannerImageUrl: string;
+  rating: number;
+  reviewCount: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+const ACTIVITY_LIST: ActivityManagementCardProps[] = [
+  {
+    id: 11,
+    userId: 0,
+    title: "string",
+    description: "string",
+    category: "string",
+    price: 0,
+    address: "string",
+    bannerImageUrl: "string",
+    rating: 0,
+    reviewCount: 0,
+    createdAt: "2024-06-03T07:50:30.593Z",
+    updatedAt: "2024-06-03T07:50:30.593Z",
+  },
+  {
+    id: 22,
+    userId: 0,
+    title: "string",
+    description: "string",
+    category: "string",
+    price: 0,
+    address: "string",
+    bannerImageUrl: "string",
+    rating: 0,
+    reviewCount: 0,
+    createdAt: "2024-06-03T07:50:30.593Z",
+    updatedAt: "2024-06-03T07:50:30.593Z",
+  },
+  {
+    id: 33,
+    userId: 0,
+    title: "string",
+    description: "string",
+    category: "string",
+    price: 0,
+    address: "string",
+    bannerImageUrl: "string",
+    rating: 0,
+    reviewCount: 0,
+    createdAt: "2024-06-03T07:50:30.593Z",
+    updatedAt: "2024-06-03T07:50:30.593Z",
+  },
+];
+export default ACTIVITY_LIST;
