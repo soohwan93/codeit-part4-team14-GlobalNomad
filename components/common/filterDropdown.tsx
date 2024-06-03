f1f4f0f9e4733507fd824e1b8a1a604dc2aa9fe5
Taeuk471 <asky990223@gmail.com>
2024-06-03 17:34:42 +0900

    :fix: 관련 이슈 수정

--- components/common/filterDropdown.tsx
+++ components/common/filterDropdown.tsx
@@ -28,11 +28,11 @@ const FilterDropdown = ({
         className="bg-white w-[40px] h-[40px] md:w-[160px] md:h-[53px] p-2 rounded-lg border border-emerald-800"
         onClick={() => setDropdownOpen(!dropdownOpen)}
       >
-        <div className="flex items-center px-2 justify-between">
+        <div className="flex items-center px-2 md:justify-between">
           <p className="hidden text-emerald-800 font-bold md:block">
             {selectedOptionLabel}
           </p>
-          <p className="text-emerald-700 font-bold">▽</p>
+          <div className={`bg-[url('/icons/chevron_down.svg')] bg-center bg-no-repeat ${dropdownOpen ? "rotate-180" : "rotate-0"}`}></div>
         </div>
       </button>
       {dropdownOpen && (
@@ -43,8 +43,8 @@ const FilterDropdown = ({
               key={option.value}
               className={`flex items-center justify-center border px-4 py-3 hover:bg-gray-100 ${
                 selectedFilter === option.value
-                  ? "font-bold text-emerald-800"
-                  : ""
+                  ? "bg-nomad-black text-white hover:bg-nomad-black"
+                  : "text-black hover:bg-gray-30"
               }`}
               onClick={() => {
                 onFilterChange(option.value);
