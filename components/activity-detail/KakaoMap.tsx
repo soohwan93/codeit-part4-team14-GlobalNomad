993f8dbdf7db52af928ada85ced00a79d910b450
Rhajiit <ksd9638@gmail.com>
2024-06-03 13:14:44 +0900

    🚧 스크립트 간략하게 작성, 수정 필요

--- components/activity-detail/KakaoMap.tsx
+++ components/activity-detail/KakaoMap.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import React from "react";
 
 type Props = {};
@@ -5,9 +6,19 @@ type Props = {};
 const KakaoMap = (props: Props) => {
   return (
     <div>
-      <div className="h-96 w-96 outline-[1px] outline-black">
+      <div id="map" className="h-96 w-96 outline-[1px] outline-black">
         카카오 맵 들어갈 자리
       </div>
+      <Script
+        type="text/javascript"
+        src="//dapi.kakao.com/v2/maps/sdk.js?appkey=da964a62e055d3e70fdf6f70db10c052"
+      />
+      <script>
+		var container = document.getElementById('map');
+		var options = {center: new kakao.maps.LatLng(33.450701, 126.570667), level: 3};
+
+		var map = new kakao.maps.Map(container, options);
+	</script>
       <span>현재 체험의 주소 들어갈 자리</span>
     </div>
   );
