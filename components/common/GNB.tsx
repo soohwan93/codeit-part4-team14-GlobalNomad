cefebcb00ee569beaa3bb2480cd27d482d421297
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-06-04 18:32:40 +0900

    ♻️ gnb 최대 길이 수정

--- components/common/GNB.tsx
+++ components/common/GNB.tsx
@@ -5,14 +5,17 @@ import Link from "next/link";
 
 const GNB = () => {
   return (
-    <header className="flex h-[70px] items-center border-b-2 border-gray-20">
-      <div className="flex w-full justify-between px-6">
-        <Link href="/">
-          <HeaderLogoSvg />
-        </Link>
-        <NavButtons />
-      </div>
-    </header>
+    <>
+      <header className="mx-auto flex h-[70px] w-full max-w-[1200px] flex-col items-center justify-center ">
+        <div className="flex w-full justify-between px-6">
+          <Link href="/">
+            <HeaderLogoSvg />
+          </Link>
+          <NavButtons />
+        </div>
+      </header>
+      <div className="border-b-2 border-gray-200" />
+    </>
   );
 };
 
