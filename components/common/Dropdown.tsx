4b4fa59af35fd770515711fdf2f1ffe0623fe787
Taeuk471 <asky990223@gmail.com>
2024-06-04 16:53:17 +0900

    :recycle: 선택시 디자인 UI 변경

--- components/common/Dropdown.tsx
+++ components/common/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 
 interface Option {
   label: string;
@@ -18,6 +18,13 @@ const Dropdown = ({
   dropdownOpen,
   setDropdownOpen,
 }: DropdownProps) => {
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
+  const handleOptionClick = (value: string) => {
+    setSelectedOption(value);
+    setDropdownOpen(false);
+  };
+
   return (
     <div className="relative">
       <div className="cursor-pointer">
@@ -28,8 +35,12 @@ const Dropdown = ({
           {Options.map((option) => (
             <button
               key={option.value}
-              className="flex items-center justify-center border px-4 py-3 hover:bg-gray-100 text-black"
-              onClick={() => setDropdownOpen(false)}
+              className={`flex items-center justify-center border px-4 py-3 hover:bg-gray-100 ${
+                selectedOption === option.value
+                  ? "bg-nomad-black text-white hover:bg-nomad-black"
+                  : "text-black hover:bg-gray-30"
+              }`}
+              onClick={() => handleOptionClick(option.value)}
             >
               {option.label}
             </button>
