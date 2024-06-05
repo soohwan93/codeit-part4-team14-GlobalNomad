b3094b5003f64e0f6f4245107fb05c481b8a3484
Rhajiit <ksd9638@gmail.com>
2024-06-05 16:30:06 +0900

    ✨ 등록된 후기가 없을 때 대응하는 문구 추가

--- components/activity-detail/ReviewList/index.tsx
+++ components/activity-detail/ReviewList/index.tsx
@@ -6,18 +6,28 @@ import Pagination from "@/components/common/Pagination";
 type Props = {};
 
 const ReviewList = (props: Props) => {
-  const reviewList: any[] = [1, 2, 3];
+  const reviewList: any[] = [];
+  const isReviewExist = reviewList.length !== 0 ? true : false;
+
   return (
     <div className="mb-36 md:mb-72">
       <div className="mb-10 md:mb-20">
-        {reviewList.map((item, i) => {
-          return (
-            <React.Fragment key={item}>
-              <Review />
-              {i !== reviewList.length - 1 && <hr className="bg-nomad-black" />}
-            </React.Fragment>
-          );
-        })}
+        {isReviewExist ? (
+          reviewList.map((item, i) => {
+            return (
+              <React.Fragment key={item}>
+                <Review />
+                {i !== reviewList.length - 1 && (
+                  <hr className="bg-nomad-black" />
+                )}
+              </React.Fragment>
+            );
+          })
+        ) : (
+          <div className="mt-10 text-center text-lg md:mt-20">
+            현재 등록된 후기가 없습니다.
+          </div>
+        )}
       </div>
       <Pagination count={3} onPageClick={() => {}} pageItemLimit={3} />
     </div>
