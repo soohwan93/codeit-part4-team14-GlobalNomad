f7d39cb7895ebbd55c830b7c99f29d1a3ca400ec
Rhajiit <ksd9638@gmail.com>
2024-06-05 15:59:18 +0900

    ✨ 에러 페이지 구성

--- app/(app)/activity-detail/[activityId]/error.tsx
+++ app/(app)/activity-detail/[activityId]/error.tsx
@@ -6,8 +6,14 @@ type Props = {};
 
 const error = (props: Props) => {
   return (
-    <div>
-      <Link href="http://localhost:3000/activity-detail/991">error</Link>
+    <div className="flex h-screen w-screen flex-col items-center justify-center text-2xl font-bold text-nomad-black">
+      <h1>해당 체험을 찾을 수 없습니다.</h1>
+      <Link
+        className="txt-blue-600 font-medium hover:underline dark:text-blue-500"
+        href="http://localhost:3000/activity-detail/991"
+      >
+        체험 있는 페이지로 이동하기
+      </Link>
     </div>
   );
 };
