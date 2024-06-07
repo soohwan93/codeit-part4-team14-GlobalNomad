36c760b03c499d74eb9fa1f1087f3d0425326684
Rhajiit <ksd9638@gmail.com>
2024-06-07 17:00:39 +0900

    💄 subImage를 통한 bannerImage 변경시, 서서히 투명도를 전환하도록 함

--- components/activity-detail/BannerImage.tsx
+++ components/activity-detail/BannerImage.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 
 interface SubImages {
   id: number;
@@ -13,26 +13,29 @@ interface BannerImageProps {
 }
 
 const BannerImage = ({ banner, subImages }: BannerImageProps) => {
-  const [currentBanner, setCurrentBanner] = useState(banner);
-  const [isMainBannerDisappear, setIsMainBannerDisappear] = useState(false);
+  const [currentBanner, setCurrentBanner] = useState<number | null>(null);
 
   return (
     <section className="max-h-[50rem] w-full">
       <div className="relative mb-2 h-[25rem] w-full overflow-hidden rounded-xl xl:h-[31.25rem]">
         <Image
-          src={currentBanner}
-          alt=""
-          fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="object-cover object-center"
-        />
-        <Image
           src={banner}
           alt="mainImage"
           fill
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className={`object-cover object-center duration-500 ${isMainBannerDisappear ? "opacity-0" : "opacity-100"}`}
+          className="object-cover object-center"
         />
+        {subImages.map((item, i) => (
+          <React.Fragment key={`banner-${item.id}`}>
+            <Image
+              src={item.imageUrl}
+              alt=""
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className={`object-cover object-center  duration-500 ${currentBanner !== item.id ? "opacity-0" : "opacity-100"}`}
+            />
+          </React.Fragment>
+        ))}
       </div>
       <section className="flex h-[8.4rem] grid-cols-4 gap-2 overflow-x-scroll md:grid xl:h-[14rem]">
         {subImages.length !== 0 &&
@@ -41,10 +44,9 @@ const BannerImage = ({ banner, subImages }: BannerImageProps) => {
               key={`subImage-${item.id}`}
               className="relative w-full min-w-[10.9375rem] overflow-hidden rounded-xl "
               onMouseOver={() => {
-                setCurrentBanner(item.imageUrl);
-                setIsMainBannerDisappear(true);
+                setCurrentBanner(item.id);
               }}
-              onMouseOut={() => setIsMainBannerDisappear(false)}
+              onMouseOut={() => setCurrentBanner(null)}
             >
               <Image
                 src={item.imageUrl}
