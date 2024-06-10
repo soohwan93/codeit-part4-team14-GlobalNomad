127781c633631b4c382da302132c684892235b5a
Taeuk471 <asky990223@gmail.com>
2024-06-10 18:54:27 +0900

    :bug: 별 모양 문제사항 변경

--- components/common/ReviewModal/Star.tsx
+++ components/common/ReviewModal/Star.tsx
@@ -1,29 +1,22 @@
 import React from "react";
 
-interface StarProps {
+type StarProps = {
   filled: boolean;
-  half?: boolean;
   onClick: () => void;
-}
+};
 
-const Star = ({ filled, half, onClick } : StarProps) => {
+const Star = ({ filled, onClick }: StarProps) => {
   return (
     <svg
       onClick={onClick}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      fill={filled ? "gold" : half ? "url(#half)" : "gray"}
+      fill={filled ? "gold" : "gray"}
       width="56px"
       height="56px"
       className="cursor-pointer"
       style={{ margin: "0 2px" }}
     >
-      <defs>
-        <linearGradient id="half">
-          <stop offset="50%" stopColor="gold" />
-          <stop offset="50%" stopColor="gray" />
-        </linearGradient>
-      </defs>
       <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
     </svg>
   );
