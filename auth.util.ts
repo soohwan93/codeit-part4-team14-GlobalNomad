7fde2901a5bb8dabc48f0594cade3de6c40aefde
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-06-12 16:36:25 +0900

    ✨ next-auth에서 사용되는 util 관련 함수 파일 작업

---
+++ auth.util.ts
@@ -0,0 +1,51 @@
+import { jwtDecode } from "jwt-decode";
+import { JWT } from "next-auth/jwt";
+
+export async function refreshAccessToken(token: JWT) {
+  try {
+    const refreshTokenExpiry = getTokenExpiry(token.refreshToken as string);
+
+    // 현재 시간이 refreshToken의 만료 시간보다 크면 만료된 것임
+    if (Date.now() > +refreshTokenExpiry! * 1000) {
+      return {
+        ...token,
+        error: "RefreshTokenExpired",
+      };
+    }
+
+    const response = await fetch(
+      "https://sp-globalnomad-api.vercel.app/4-14/auth/tokens",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token.refreshToken}`,
+        },
+      },
+    );
+
+    const refreshedTokens = await response.json();
+    const accessTokenExpiry = getTokenExpiry(refreshedTokens.accessToken);
+    if (!response.ok) {
+      throw refreshedTokens;
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.accessToken,
+      accessTokenExpires: +accessTokenExpiry! * 1000,
+    };
+  } catch (error) {
+    console.error("Error refreshing access token:", error);
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
+
+// Refresh token의 만료 시간을 디코딩하여 가져오는 함수
+export function getTokenExpiry(token: string): number {
+  const decodedToken = jwtDecode(token);
+  return decodedToken?.exp!;
+}
