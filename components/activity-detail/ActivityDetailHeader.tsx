385ab7888d6dad964ae52063588d8c672a080257
Rhajiit <ksd9638@gmail.com>
2024-06-13 15:00:44 +0900

    ✨ activity-detail의 dropdown 함수를 구체화

--- components/activity-detail/ActivityDetailHeader.tsx
+++ components/activity-detail/ActivityDetailHeader.tsx
@@ -1,13 +1,19 @@
 "use client";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import KebabSvg from "../common/svg/KebabSvg";
 import Dropdown from "../common/Dropdown";
 import { useRouter } from "next/navigation";
 import DeletePopup from "../common/Popup/DeletePopup";
+import NotificationPopup from "../common/Popup/NotificationPopup";
+import { deleteMyActivity } from "@/util/api";
 
 const ActivityDetailHeader = ({ activityId }: { activityId: number }) => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [isDeletePopupOpen, SetIsDeletePopupOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [poopUpMessage, setPopupMessage] = useState("");
+  const [isDeletePopupOpen, setIsDeletePopupOpen] = useState(false);
+
+  const deleteSuccess = useRef(false);
   const router = useRouter();
 
   const handleDropdownEdit = () => {
@@ -15,10 +21,31 @@ const ActivityDetailHeader = ({ activityId }: { activityId: number }) => {
   };
 
   const handleDropdownDelete = () => {
-    SetIsDeletePopupOpen(true);
+    setIsDeletePopupOpen(true);
   };
 
-  const handleActivityDelete = async () => {};
+  const handlePopupClose = () => {
+    setIsPopupOpen(false);
+    if (deleteSuccess.current) {
+      router.push("/");
+    }
+  };
+
+  const handleActivityDelete = async () => {
+    setIsDeletePopupOpen(false);
+
+    try {
+      await deleteMyActivity(activityId);
+      deleteSuccess.current = true;
+      setPopupMessage("이 활동을 성공적으로 삭제했어요!");
+      setIsPopupOpen(true);
+    } catch {
+      setPopupMessage(
+        "예기치 못한 오류로 활동을 삭제하지 못했어요. 잠시후 다시 시도해 주세요.",
+      );
+      setIsPopupOpen(true);
+    }
+  };
 
   const dropdownProps = [
     { label: "수정하기", value: "수정하기", onClick: handleDropdownEdit },
@@ -29,22 +56,30 @@ const ActivityDetailHeader = ({ activityId }: { activityId: number }) => {
     <>
       <Dropdown
         defaultLabel={
-          <button type="button" onClick={() => setDropdownOpen(!dropdownOpen)}>
+          <button
+            type="button"
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          >
             <KebabSvg />
           </button>
         }
         originPositionRight
-        dropdownOpen={dropdownOpen}
-        setDropdownOpen={setDropdownOpen}
+        dropdownOpen={isDropdownOpen}
+        setDropdownOpen={setIsDropdownOpen}
         Options={dropdownProps}
       />
       <DeletePopup
         isOpen={isDeletePopupOpen}
         onClose={() => {
-          SetIsDeletePopupOpen(false);
+          setIsDeletePopupOpen(false);
         }}
         onDelete={handleActivityDelete}
         message="정말 이 활동을 삭제하시겠어요?"
+      />
+      <NotificationPopup
+        message={poopUpMessage}
+        isOpen={isPopupOpen}
+        onClose={handlePopupClose}
       />
     </>
   );
