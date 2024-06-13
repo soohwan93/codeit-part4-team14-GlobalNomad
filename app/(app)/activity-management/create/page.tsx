58e4fe381bbbbcd220edfdc8f0d37ce5f6036cf4
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-06-13 18:22:38 +0900

    ✨ 내 체험 등록 작업

--- app/(app)/activity-management/create/page.tsx
+++ app/(app)/activity-management/create/page.tsx
@@ -1,9 +1,18 @@
+import ActivityCreateForm from "@/components/activity-management/create/ActivityCreateForm";
+import ActivityCreateHeader from "@/components/activity-management/create/ActivityCreateHeader";
+import CreateMain from "@/components/activity-management/create/CreateMain";
+import CreateWrapper from "@/components/activity-management/create/CreateWrapper";
+import Input from "@/components/common/Input";
 import React from "react";
 
 type Props = {};
 
 const page = (props: Props) => {
-  return <div>page</div>;
+  return (
+    <CreateWrapper>
+      <CreateMain />
+    </CreateWrapper>
+  );
 };
 
 export default page;
