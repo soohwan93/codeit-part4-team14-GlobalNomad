cfd56aeac027629f6d45b8799788d5ea0139228f
Rhajiit <ksd9638@gmail.com>
2024-06-17 15:26:52 +0900

    🏷️ 추후 작업 용이성을 위한 타입 지정

--- app/(app)/reservation-status/page.tsx
+++ app/(app)/reservation-status/page.tsx
@@ -1,8 +1,29 @@
+import ReservationStatus from "@/components/reservation-status/ReservationStatus";
 import { getMyActivities } from "@/util/api";
 import React from "react";
 
+interface ActivityData {
+  address: string;
+  bannerImageUrl: string;
+  category: string;
+  createdAt: string;
+  description: string;
+  id: number;
+  price: number;
+  rating: number;
+  reviewCount: number;
+  title: string;
+  updatedAt: string;
+  userId: number;
+}
+interface myActivityListTypes {
+  activities: ActivityData[];
+  cursorId: number | null;
+  totalCount: number;
+}
+
 const page = async () => {
-  const myActivityList = await getMyActivities();
+  const myActivityList: myActivityListTypes = await getMyActivities();
 
   return (
     <div className="h-screen w-screen bg-gray-10 px-8 pt-16">
@@ -10,6 +31,7 @@ const page = async () => {
         <h2 className="mb-8 text-[2rem] font-bold leading-normal text-black">
           예약 현황
         </h2>
+        <ReservationStatus myActivityList={myActivityList.activities} />
       </main>
     </div>
   );
--- components/reservation-status/ReservationStatus.tsx
+++ components/reservation-status/ReservationStatus.tsx
@@ -1,7 +1,26 @@
 "use client";
 import React from "react";
 
-const ReservationStatus = () => {
+interface ActivityData {
+  address: string;
+  bannerImageUrl: string;
+  category: string;
+  createdAt: string;
+  description: string;
+  id: number;
+  price: number;
+  rating: number;
+  reviewCount: number;
+  title: string;
+  updatedAt: string;
+  userId: number;
+}
+
+const ReservationStatus = ({
+  myActivityList,
+}: {
+  myActivityList: ActivityData[];
+}) => {
   return <div className="h-96 w-full border border-black"></div>;
 };
 
