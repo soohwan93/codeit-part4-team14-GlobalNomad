4412872c334958ed73c4e729af6815bcb73e0997
Rhajiit <ksd9638@gmail.com>
2024-06-17 15:03:27 +0900

    ✅ error 페이지의 디버깅 기능 삭제

--- app/(app)/activity-detail/[activityId]/error.tsx
+++ app/(app)/activity-detail/[activityId]/error.tsx
@@ -7,18 +7,12 @@ type Props = {};
 const error = (props: Props) => {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center text-2xl font-bold text-nomad-black">
-      <h1>해당 체험을 찾을 수 없습니다.</h1>
+      <h1>해당 체험을 찾을 수 없거나, 예기치 못한 에러가 발생했습니다.</h1>
       <Link
         className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-        href="http://localhost:3000/activity-detail/991"
+        href="/"
       >
-        체험 있는 페이지로 이동하기
-      </Link>
-      <Link
-        className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-        href="http://localhost:3000/activity-detail/993"
-      >
-        체험 없는 페이지로 이동하기
+        메인 페이지로 이동하기
       </Link>
     </div>
   );
