e248a4aab345cee87771b8f2e7e2e00a3dc60ecd
Rhajiit <ksd9638@gmail.com>
2024-06-17 14:14:06 +0900

    💄 reservationStatus 페이지 간단한 ui 구성

--- app/(app)/reservation-status/page.tsx
+++ app/(app)/reservation-status/page.tsx
@@ -1,9 +1,18 @@
+import { getMyActivities } from "@/util/api";
 import React from "react";
 
-type Props = {};
+const page = async () => {
+  const myActivityList = await getMyActivities();
 
-const page = (props: Props) => {
-  return <div>page</div>;
+  return (
+    <div className="h-screen w-screen bg-gray-10 px-8 pt-16">
+      <main className="mx-auto w-full max-w-[75rem]">
+        <h2 className="mb-8 text-[2rem] font-bold leading-normal text-black">
+          예약 현황
+        </h2>
+      </main>
+    </div>
+  );
 };
 
 export default page;
---
+++ components/reservation-status/ReservationStatus.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const ReservationStatus = () => {
+  return <div className="h-96 w-full border border-black"></div>;
+};
+
+export default ReservationStatus;
