af639d4d358f4d54a5c1d50f381c4ac1955e36bc
Rhajiit <ksd9638@gmail.com>
2024-06-18 15:29:10 +0900

    ✨ 배열을 활용해 각 날짜에 받아온 response를 할당할 수 있도록 함.

--- components/reservation-status/ReservationCalendar.tsx
+++ components/reservation-status/ReservationCalendar.tsx
@@ -1,37 +1,85 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import useCalendar from "./useCalendar";
 import { subMonths } from "date-fns";
+import { getMyActivityReservationDashBoard } from "@/util/api";
 
 interface ReservationCalendarProps {
   selectedActivityId: number;
 }
+
+interface ReservationStatusOfMonth {
+  date: string;
+  reservations: {
+    completed: number;
+    confirmed: number;
+    pending: number;
+  };
+}
+
 const DAY_LIST = ["SUN", "MON", "TUE", "WED", "THUR", "FRI", "SAT"];
 
 const ReservationCalendar = ({
   selectedActivityId,
 }: ReservationCalendarProps) => {
   const calendar = useCalendar();
+  const [reservationStatusOfMonth, setReservationStatusOfMonth] = useState<
+    ReservationStatusOfMonth[]
+  >([]);
+  const currentMonth = useRef(
+    calendar.currentDate.getFullYear() +
+      "-" +
+      String(calendar.currentDate.getMonth() + 1).padStart(2, "0") +
+      "-",
+  );
 
-  const handleMonthChange = (direction: number) => {
-    calendar.setCurrentDate(subMonths(calendar.currentDate, -direction));
+  const handleClickMonthChangeButton = async (direction: number) => {
+    setReservationStatusOfMonth([]);
+    const directionInFunc = -direction;
+    const year = subMonths(calendar.currentDate, directionInFunc).getFullYear();
+    const month = String(
+      subMonths(calendar.currentDate, directionInFunc).getMonth() + 1,
+    ).padStart(2, "0");
+
+    calendar.setCurrentDate(subMonths(calendar.currentDate, directionInFunc));
+    const response = await getMyActivityReservationDashBoard(
+      selectedActivityId,
+      { year: year, month: month },
+    );
+
+    const dateString =
+      String(year) + "-" + String(month).padStart(2, "0") + "-";
+    currentMonth.current = dateString;
+
+    let convertedArray: ReservationStatusOfMonth[] = [];
+    response.map((item: ReservationStatusOfMonth) => {
+      convertedArray[Number(item.date.split("-")[2])] = item;
+    });
+
+    setReservationStatusOfMonth(convertedArray);
+    console.log(convertedArray);
   };
+
+  useEffect(() => {
+    handleClickMonthChangeButton(0);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <section className="">
       <div className="mx-auto mb-6 mt-9 flex w-full max-w-[21.375rem] items-center justify-between py-1 text-xl font-bold leading-6 text-black">
         <button
           className="h-6 w-6 shrink-0 rotate-180 rounded-full bg-[url('/icons/arrow.svg')] bg-center bg-no-repeat hover:bg-gray-20"
-          onClick={() => handleMonthChange(-1)}
+          onClick={() => handleClickMonthChangeButton(-1)}
           type="button"
         />
         {calendar.currentDate.getFullYear() +
           "년 " +
-          calendar.currentDate.getMonth() +
+          (calendar.currentDate.getMonth() + 1) +
           "월"}
         <button
           className="h-6 w-6 shrink-0 rounded-full bg-[url('/icons/arrow.svg')] bg-center bg-no-repeat hover:bg-gray-20"
-          onClick={() => handleMonthChange(1)}
+          onClick={() => handleClickMonthChangeButton(1)}
           type="button"
         />
       </div>
@@ -48,10 +96,11 @@ const ReservationCalendar = ({
           weekItem.map((dayItem, i) =>
             dayItem !== 0 ? (
               <div
-                key={`${dayItem}일`}
+                key={currentMonth.current + String(dayItem).padStart(2, "0")}
                 className="min-h-40 flex-col justify-between bg-white p-4 font-mono text-xl font-medium leading-normal text-gray-50"
               >
                 {dayItem}
+                {reservationStatusOfMonth[dayItem] ? "asdf" : ""}
               </div>
             ) : (
               <div key={`dummyDay-${i}`} />
