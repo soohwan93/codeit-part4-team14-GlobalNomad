35e139098e82094fed370506d50979c15fa8280f
Rhajiit <ksd9638@gmail.com>
2024-06-18 18:20:15 +0900

    💄 타입 셀렉터 스타일 일부 조정

--- components/reservation-status/ReservationModalContents/ReservationTypeSelector.tsx
+++ components/reservation-status/ReservationModalContents/ReservationTypeSelector.tsx
@@ -13,35 +13,38 @@ const ReservationTypeSelector = ({
     <>
       <div className="grid w-fit grid-cols-3">
         <button
-          className="flex h-10 flex-col justify-between"
+          className="group flex h-10 flex-col justify-between"
           onClick={() => setType("pending")}
           type="button"
         >
           <span className="col-start-1 row-start-1 px-3 py-1">신청</span>
           <div
-            className={`w-full rounded-t bg-nomad-black duration-200 ${type === "pending" ? "h-1" : "h-0"}`}
+            className={`w-full rounded-t border-[1px] border-white duration-200 ${type === "pending" ? "h-1 bg-nomad-black" : "h-0"} 
+            group-hover:h-full group-hover:border-nomad-black`}
           />
         </button>
 
         <button
-          className="flex h-10 flex-col justify-between"
+          className="group flex h-10 flex-col justify-between"
           onClick={() => setType("confirmed")}
           type="button"
         >
           <span className="col-start-1 row-start-1 px-3 py-1">승인</span>
           <div
-            className={`w-full rounded-t bg-nomad-black duration-200 ${type === "confirmed" ? "h-1" : "h-0"}`}
+            className={`w-full rounded-t border-[1px] border-white duration-200 ${type === "confirmed" ? "h-1 bg-nomad-black" : "h-0"} 
+            group-hover:h-full group-hover:border-nomad-black`}
           />
         </button>
 
         <button
-          className="flex h-10 flex-col justify-between"
+          className="group flex h-10 flex-col justify-between"
           onClick={() => setType("denied")}
           type="button"
         >
           <span className="col-start-1 row-start-1 px-3 py-1">거절</span>
           <div
-            className={`w-full rounded-t bg-nomad-black duration-200 ${type === "denied" ? "h-1" : "h-0"}`}
+            className={`w-full rounded-t border-[1px] border-white duration-200 ${type === "denied" ? "h-1 bg-nomad-black" : "h-0"} 
+                        group-hover:h-full group-hover:border-nomad-black`}
           />
         </button>
       </div>
--- components/reservation-status/ReservationModalContents/index.tsx
+++ components/reservation-status/ReservationModalContents/index.tsx
@@ -8,9 +8,6 @@ interface ReservationModalContents {
 
 const ReservationModalContents = ({ type }: ReservationModalContents) => {
   const [currentModalType, setCurrentModalType] = useState(type);
-  const [currentCursorHover, setCurrentCursorHover] = useState<null | string>(
-    null,
-  );
 
   return (
     <div className="max-h-[43.75rem]">
