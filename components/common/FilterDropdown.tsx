217dc6441433083b3adc70defa9cba05ecdaac7b
Taeuk471 <asky990223@gmail.com>
2024-06-19 11:21:02 +0900

    :recycle: 빌드 관련 오류 해결

--- components/common/FilterDropdown.tsx
+++ components/common/FilterDropdown.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import Dropdown from "./dropdown"; 
+import Dropdown from "./Dropdown";
+ 
 
 interface FilterOption {
   label: string;
@@ -27,26 +28,21 @@ const FilterDropdown = ({
   return (
     <Dropdown
       Options={filterOptions}
-      defaultLabel={
-        <button
-          className="bg-white w-[40px] h-[40px] md:w-[160px] md:h-[53px] p-2 rounded-lg border border-emerald-800"
-          onClick={() => setDropdownOpen(!dropdownOpen)}
-        >
-          <div className="flex items-center px-2 md:justify-between">
-            <p className="hidden text-emerald-800 font-bold md:block">
-              {selectedOptionLabel}
-            </p>
-            <div
-              className={`bg-[url('/icons/chevron_down.svg')] bg-center bg-no-repeat ${
-                dropdownOpen ? "rotate-180" : "rotate-0"
-              }`}
-            ></div>
-          </div>
-        </button>
-      }
+      defaultLabel={<button
+        className="bg-white w-[40px] h-[40px] md:w-[160px] md:h-[53px] p-2 rounded-lg border border-emerald-800"
+        onClick={() => setDropdownOpen(!dropdownOpen)}
+      >
+        <div className="flex items-center px-2 md:justify-between">
+          <p className="hidden text-emerald-800 font-bold md:block">
+            {selectedOptionLabel}
+          </p>
+          <div
+            className={`bg-[url('/icons/chevron_down.svg')] bg-center bg-no-repeat ${dropdownOpen ? "rotate-180" : "rotate-0"}`}
+          ></div>
+        </div>
+      </button>}
       dropdownOpen={dropdownOpen}
-      setDropdownOpen={setDropdownOpen} 
-    />
+      setDropdownOpen={setDropdownOpen} originPositionRight={false}    />
   );
 };
 
