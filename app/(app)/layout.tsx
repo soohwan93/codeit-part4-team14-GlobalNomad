697216db9d25e82aac6382374a991cc5e234feb7
Taeuk471 <asky990223@gmail.com>
2024-06-19 11:09:20 +0900

    :recycle: change layoutfile

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -5,6 +5,8 @@ import GNB from "@/components/common/GNB";
 import Footer from "@/components/common/Footer";
 import { SessionProviderWrapper } from "@/contexts/SessionProviderWrapper";
 import { SessionProvider } from "next-auth/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 const pretendard = localFont({
   src: "../../public/fonts/PretendardVariable.woff2",
@@ -23,16 +25,21 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // Create a client
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <html lang="ko">
       <body className={pretendard.className}>
         <SessionProvider>
-          <SessionProviderWrapper>
-            <GNB />
-            {children}
-            <Footer />
-            <div id="portal"></div>
-          </SessionProviderWrapper>
+          <QueryClientProvider client={queryClient}>
+            <SessionProviderWrapper>
+              <GNB />
+              {children}
+              <Footer />
+              <div id="portal"></div>
+            </SessionProviderWrapper>
+          </QueryClientProvider>
         </SessionProvider>
       </body>
     </html>
