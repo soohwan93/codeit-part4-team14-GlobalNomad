1cb0f40f00085005ff6ee6f7b48cfdaabc823c6c
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-06-19 15:02:37 +0900

    ✨ 예약내역 정보를 api에 맞게 format하는 함수 추가

---
+++ util/formatSchedule.ts
@@ -0,0 +1,23 @@
+import { ScheduleType } from "@/components/activity-management/create/ReservationTimeInputs";
+
+export const formatSchedule = (schedule: ScheduleType) => {
+  const { date, startTime, endTime } = schedule;
+
+  const dateMonth = date.month >= 10 ? date.month : `0${date.month}`;
+
+  const formatHourOrMinute = (hourOrMinute: number) => {
+    const formattedValue =
+      hourOrMinute >= 10 ? hourOrMinute : `0${hourOrMinute}`;
+    return formattedValue;
+  };
+
+  const formattedDate = `${date.year}-${dateMonth}-${date.day}`;
+  const formattedStartTime = `${formatHourOrMinute(startTime.hour)}:${formatHourOrMinute(startTime.minute)}`;
+  const formattedEndTime = `${formatHourOrMinute(endTime.hour)}:${formatHourOrMinute(endTime.minute)}`;
+
+  return {
+    date: formattedDate,
+    startTime: formattedStartTime,
+    endTime: formattedEndTime,
+  };
+};
