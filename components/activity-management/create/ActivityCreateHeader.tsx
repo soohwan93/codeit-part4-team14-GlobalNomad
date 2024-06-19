5fbd4985321c73906593312ac7e6d2623d224e1a
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-06-19 14:51:44 +0900

    ✨ 체험 생성 header 컴포넌트 작성

--- components/activity-management/create/ActivityCreateHeader.tsx
+++ components/activity-management/create/ActivityCreateHeader.tsx
@@ -1,21 +1,18 @@
 import Button from "@/components/common/Button";
-import React from "react";
 
-type Props = {};
+interface Props {
+  isDisabled: boolean;
+}
 
-const ActivityCreateHeader = (props: Props) => {
-  const handleCreate = () => {
-    alert("내 체험 등록!");
-  };
+const ActivityCreateHeader = ({ isDisabled }: Props) => {
   return (
     <div className="flex items-center justify-between pb-6">
       <div className="text-[32px] font-bold">내 체험 등록</div>
-      <Button
-        type="submit"
-        className="h-12 w-[120px] rounded-[4px] bg-nomad-black font-medium text-white"
-      >
-        등록하기
-      </Button>
+      <div className="h-12 w-[120px] rounded-[4px] font-medium text-white">
+        <Button disabled={isDisabled} type="submit">
+          등록하기
+        </Button>
+      </div>
     </div>
   );
 };
