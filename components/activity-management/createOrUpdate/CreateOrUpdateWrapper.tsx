3792f52072ec08d9c52a2fa6e425f7c4b69b8f0f
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-06-21 05:10:20 +0900

    💄 스타일 변경

--- components/activity-management/createOrUpdate/CreateOrUpdateWrapper.tsx
+++ components/activity-management/createOrUpdate/CreateOrUpdateWrapper.tsx
@@ -4,7 +4,7 @@ interface Props {
 }
 const CreateOrUpdateWrapper = ({ children }: Props) => {
   return (
-    <div className="flex items-center justify-center bg-gray-10 py-6 md:py-20">
+    <div className="flex items-center justify-center bg-gray-10 py-6 md:py-12">
       <div className="flex w-[768px] flex-col gap-5 px-4">{children}</div>
     </div>
   );
