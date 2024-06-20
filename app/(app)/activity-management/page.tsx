a5b23be8a66f386d00b6a6dea42ef0dd7ac5e745
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-06-21 05:06:31 +0900

    ♻️ 불필요한 코드 제거 및 데이터 server에서 불러오는 코드 추가

--- app/(app)/activity-management/page.tsx
+++ app/(app)/activity-management/page.tsx
@@ -1,31 +1,21 @@
-// "use client";
-import ApiTest1 from "@/components/test/ApiTest1";
-import ApiTest2 from "@/components/test/ApiTest2";
-import ColorTest from "@/components/test/ColorTest";
-import {
-  getMyActivities,
-  getMyActivityReservationDashBoard,
-  postLogin,
-} from "@/util/api";
-import type { LoginBody, MyActivitiesQuery } from "@/util/apiType";
+import { getMyActivities } from "@/util/api";
+import type { MyActivitiesQuery } from "@/util/apiType";
 
 import React from "react";
-import SubButtonTest from "@/components/test/SubButtonTest";
-import LogoTest from "@/components/test/LogoTest";
-import ActivityManagementCardWrapper from "@/components/activity-management/ActivityManagementCardWrapper";
-import LoginTest from "@/components/test/LoginTest";
-import { Button } from "flowbite-react";
 import ActivityManagementHeader from "@/components/activity-management/ActivityManagementHeader";
-import { cookies } from "next/headers";
+
 import ActivityManagementWrapper from "@/components/activity-management/ActivityManagementWrapper";
+import ActivityManagementCardWrapper from "@/components/activity-management/ActivityManagementCardWrapper";
 
-type Props = {};
-
-const page = async (props: Props) => {
+const page = async () => {
+  const query: MyActivitiesQuery = {
+    size: 5,
+  };
+  const { activities } = await getMyActivities(query);
   return (
     <ActivityManagementWrapper>
       <ActivityManagementHeader />
-      <ActivityManagementCardWrapper />
+      <ActivityManagementCardWrapper initialActivities={activities} />
     </ActivityManagementWrapper>
   );
 };
