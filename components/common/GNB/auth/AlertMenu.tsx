70b586036a4170189eacabc982795c024b564ef6
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-06-20 17:53:27 +0900

    ♻️ api response 관련 타입 추가 및 초기 데이터를 state로 설정

--- components/common/GNB/auth/AlertMenu.tsx
+++ components/common/GNB/auth/AlertMenu.tsx
@@ -1,67 +1,44 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import AlertSvg from "../../svg/AlertSvg";
+import NotificationModal, {
+  Notification,
+} from "../../NotificationModal/NotificationModal";
 
-type Props = {};
+interface ResponseNotificationsApi {
+  totalCount: 0;
+  notifications: Notification[];
+  cursorId: number | null;
+}
 
-const AlertMenu = (props: Props) => {
+type Props = {
+  notificationsFromApi: ResponseNotificationsApi;
+};
+
+const AlertMenu = ({ notificationsFromApi }: Props) => {
+  const [notifications, setNotifications] = useState<Notification[]>(
+    notificationsFromApi?.notifications || [],
+  );
   const [isAlertClicked, setIsAlertClicked] = useState(false);
-  const alertRef = useRef<HTMLDivElement>(null);
 
-  const handleAlertClick = () => {
+  const handleAlertClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation(); // 이벤트 버블링 막기
     setIsAlertClicked(!isAlertClicked);
   };
-
-  const handleClickAlertOutside = (e: MouseEvent) => {
-    if (
-      alertRef.current &&
-      !alertRef.current.contains(e.target as Node) &&
-      !(e.target as Element).closest("#alertSvg")
-    ) {
-      setIsAlertClicked(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickAlertOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickAlertOutside);
-    };
-  }, []);
-
   return (
     <>
-      <div ref={alertRef} className="relative flex">
-        <AlertSvg onClick={handleAlertClick} isClicked={isAlertClicked} />
-        <ul
-          className={`absolute ${isAlertClicked ? `block` : `hidden`} right-0 mt-7 w-24 transform text-gray-700 transition-transform duration-300 ease-in-out dark:text-gray-300 md:left-0`}
-        >
-          <li className="">
-            <a
-              className="block whitespace-nowrap rounded-t bg-gray-200 px-4 py-2 hover:bg-gray-400 dark:bg-gray-600 dark:hover:bg-gray-700"
-              href="#"
-            >
-              임시 알림
-            </a>
-          </li>
-          <li className="">
-            <a
-              className="block whitespace-nowrap bg-gray-200 px-4 py-2 hover:bg-gray-400 dark:bg-gray-600 dark:hover:bg-gray-700"
-              href="#"
-            >
-              임시 알림
-            </a>
-          </li>
-          <li className="">
-            <a
-              className="block whitespace-nowrap rounded-b bg-gray-200 px-4 py-2 hover:bg-gray-400 dark:bg-gray-600 dark:hover:bg-gray-700"
-              href="#"
-            >
-              임시 알림
-            </a>
-          </li>
-        </ul>
+      <div onClick={handleAlertClick} className="relative">
+        <AlertSvg isClicked={isAlertClicked} />
+        {isAlertClicked && (
+          <NotificationModal
+            setState={setIsAlertClicked}
+            notifications={notifications}
+            setNotifications={setNotifications}
+            buttonPosition={{ left: 0, top: 22 }}
+          />
+        )}
+        <div className="absolute">{isAlertClicked}</div>
       </div>
     </>
   );
