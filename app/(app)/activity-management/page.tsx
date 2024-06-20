c990d1b22bd27293b2bde21b397b66760d2db1b2
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-06-20 15:48:16 +0900

    🔥 불필요한 코드 제거

--- app/(app)/activity-management/page.tsx
+++ app/(app)/activity-management/page.tsx
@@ -17,34 +17,16 @@ import LoginTest from "@/components/test/LoginTest";
 import { Button } from "flowbite-react";
 import ActivityManagementHeader from "@/components/activity-management/ActivityManagementHeader";
 import { cookies } from "next/headers";
+import ActivityManagementWrapper from "@/components/activity-management/ActivityManagementWrapper";
 
 type Props = {};
 
 const page = async (props: Props) => {
-  // const resActivity = await getMyActivities();
-  // console.log(resActivity);
   return (
-    <>
-      {/* <div>accessToken: {res.accessToken}</div>
-    <div>refreshToken: {res.refreshToken}</div>
-    <div>유저 이메일: {res.user.email}</div>
-  <div>유저 닉네임: {res.user.nickname}</div> */}
-      {/* <ColorTest /> */}
-      <LoginTest />
-      {/* <ApiTest1>apiGETFunctionTest</ApiTest1>
-    <ApiTest2>apiGETFunctionTest</ApiTest2> */}
-      {/* <div className="my-10">
-      <SubButtonTest />
-      </div>
-    <LogoTest /> */}
-      <div className="flex items-center justify-center bg-gray-10 py-20">
-        <div className="flex w-[768px] flex-col gap-5 px-5">
-          <ActivityManagementHeader />
-          {/* <span>{token}</span> */}
-          <ActivityManagementCardWrapper />
-        </div>
-      </div>
-    </>
+    <ActivityManagementWrapper>
+      <ActivityManagementHeader />
+      <ActivityManagementCardWrapper />
+    </ActivityManagementWrapper>
   );
 };
 
