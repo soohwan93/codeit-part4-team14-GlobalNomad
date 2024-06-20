4285a209c25564a1a02f4956cf59cfa69879e6f9
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-06-20 15:48:36 +0900

    ✨ 케밥 컴포넌트 생성

---
+++ components/activity-management/ActivityManagementKebab.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+const ActivityManagementKebab = (props: Props) => {
+  return (
+    <button
+      className="h-10 w-10 shrink-0 bg-[url('/icons/kebab.svg')] "
+      type="button"
+    ></button>
+  );
+};
+
+export default ActivityManagementKebab;
