033884bea78db8b9406a97a52e31c0ab6bc97ec0
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-06-20 14:38:28 +0900

    ♻️ api 데이터를 ScheduleType에 맞게 format하는 함수 추가

--- util/formatSchedule.ts
+++ util/formatSchedule.ts
@@ -1,4 +1,5 @@
 import { ScheduleType } from "@/components/activity-management/createOrupdate/ReservationTimeInputs";
+import { CalendarDate, ZonedDateTime } from "@internationalized/date";
 
 export const formatSchedule = (schedule: ScheduleType) => {
   const { date, startTime, endTime } = schedule;
@@ -20,4 +21,43 @@ export const formatSchedule = (schedule: ScheduleType) => {
     startTime: formattedStartTime,
     endTime: formattedEndTime,
   };
+};
+
+export const mapApiDataToSchedules = (
+  schedules: [
+    {
+      id: number;
+      date: string;
+      startTime: string;
+      endTime: string;
+    },
+  ],
+) => {
+  return schedules.map((item, index) => ({
+    key: `schedules_${index}`,
+    id: item.id,
+    date: new CalendarDate(
+      +item.date.split("-")[0],
+      +item.date.split("-")[1],
+      +item.date.split("-")[2],
+    ),
+    startTime: new ZonedDateTime(
+      +item.date.split("-")[0],
+      +item.date.split("-")[1],
+      +item.date.split("-")[2],
+      "Asia/Seoul",
+      32400000,
+      +item.startTime.split(":")[0],
+      +item.startTime.split(":")[1],
+    ),
+    endTime: new ZonedDateTime(
+      +item.date.split("-")[0],
+      +item.date.split("-")[1],
+      +item.date.split("-")[2],
+      "Asia/Seoul",
+      32400000,
+      +item.endTime.split(":")[0],
+      +item.endTime.split(":")[1],
+    ),
+  }));
 };
