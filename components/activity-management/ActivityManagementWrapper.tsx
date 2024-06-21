7d47683dcc9f17426ad728801b8c1bdf5a655d3e
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-06-21 15:05:47 +0900

    ♻️ 체험이 없는 경우도 추가

--- components/activity-management/ActivityManagementWrapper.tsx
+++ components/activity-management/ActivityManagementWrapper.tsx
@@ -4,7 +4,7 @@ type Props = { children: React.ReactNode };
 
 const ActivityManagementWrapper = ({ children }: Props) => {
   return (
-    <div className="flex items-center justify-center bg-gray-10 py-14 md:py-20">
+    <div className="min-w-screen flex min-h-screen justify-center bg-gray-10 py-14 md:py-20">
       <div className="flex w-[768px] flex-col gap-5 px-4">{children}</div>
     </div>
   );
