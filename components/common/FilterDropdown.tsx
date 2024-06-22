2189da3f75e24c414800f1c53ace02a129b94915
Taeuk471 <asky990223@gmail.com>
2024-06-22 17:45:59 +0900

    :recycle: 디자인 이슈 변경

--- components/common/FilterDropdown.tsx
+++ components/common/FilterDropdown.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Dropdown from "./Dropdown";
+import DropdownArrow from "./svg/DropdownArrowSvg";
 
 interface FilterOption {
   label: string;
@@ -42,15 +43,15 @@ const FilterDropdown = ({
           className="bg-white w-[40px] h-[40px] md:w-[160px] md:h-[53px] p-2 rounded-lg border border-emerald-800"
           onClick={() => setDropdownOpen(!dropdownOpen)}
         >
-          <div className="flex items-center px-2 md:justify-between">
+          <div className="flex items-center justify-center md:justify-between px-2">
             <p className="hidden text-emerald-800 font-bold md:block">
               {selectedOptionLabel}
             </p>
             <div
-              className={`bg-[url('/icons/chevron_down.svg')] bg-center bg-no-repeat ${
-                dropdownOpen ? "rotate-180" : "rotate-0"
-              }`}
-            ></div>
+              className={`flex h-6 w-6 items-center justify-center duration-100 md:h-5 md:w-5 ${dropdownOpen ? "rotate-180" : "rotate-0"}`}
+            >
+              <DropdownArrow />
+            </div>
           </div>
         </button>
       }
