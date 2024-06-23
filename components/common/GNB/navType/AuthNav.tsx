084fda52a283c0b958dc2b5ff51f0e656d140a00
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-06-23 20:56:58 +0900

    ♻️ route handler fetching을 적용

--- components/common/GNB/navType/AuthNav.tsx
+++ components/common/GNB/navType/AuthNav.tsx
@@ -3,20 +3,30 @@ import DivineLineSvg from "../../svg/DivineLineSvg";
 import AuthMenu from "../auth/AuthMenu";
 import AlertMenu from "../auth/AlertMenu";
 import UserMenu from "../auth/UserMenu";
-import { MyNotificationsQuery } from "@/util/apiType";
-import { getMyNotifications } from "@/util/api";
+import { auth } from "@/auth";
 
 const AuthNav = async () => {
-  const query: MyNotificationsQuery = {
-    size: 10,
-  };
-  const notifications = await getMyNotifications(query);
+  const session = await auth();
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/notifications`,
+    {
+      cache: "no-cache",
+      method: "POST",
+      body: JSON.stringify(session),
+    },
+  );
+  const { data } = await res.json();
+  const { totalCount, cursorId, notifications } = data;
   return (
     <>
       <div className="flex gap-3">
         <AuthMenu />
         <DivineLineSvg />
-        <AlertMenu notificationsFromApi={notifications} />
+        <AlertMenu
+          totalCount={totalCount}
+          cursorId={cursorId}
+          notifications={notifications}
+        />
         <DivineLineSvg />
         <UserMenu />
       </div>
