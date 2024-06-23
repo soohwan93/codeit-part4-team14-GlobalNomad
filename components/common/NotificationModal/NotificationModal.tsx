fa2dd9e3cb3378c4c40a58d46195c69f5cc48d16
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-06-23 20:57:28 +0900

    ♻️ 알림 모달 ui, dataFetching 관련 수정

--- components/common/NotificationModal/NotificationModal.tsx
+++ components/common/NotificationModal/NotificationModal.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useCallback, useRef, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { deleteMyNotification, getMyNotifications } from "@/util/api";
 import dateFormat from "@/util/dateForm";
-import AlertSvg from '@/components/common/svg/AlertSvg';
+import AlertSvg from "@/components/common/svg/AlertSvg";
 
 export interface Notification {
   id: number;
@@ -16,6 +16,8 @@ export interface Notification {
 }
 
 interface NotificationModalProps {
+  cursorId: number | null;
+  totalCount: number;
   setState: React.Dispatch<React.SetStateAction<boolean>>;
   notifications: Notification[];
   setNotifications: React.Dispatch<React.SetStateAction<Notification[]>>;
@@ -23,6 +25,8 @@ interface NotificationModalProps {
 }
 
 const NotificationModal = ({
+  cursorId: initialCursorId,
+  totalCount,
   setState,
   notifications,
   setNotifications,
@@ -32,7 +36,13 @@ const NotificationModal = ({
   const [loading, setLoading] = useState(false);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const notificationsRef = useRef<HTMLDivElement | null>(null);
-
+  const [cursorId, setCursorId] = useState<number | null>(initialCursorId);
+  const [lastCursorId, setLastCursorId] = useState<number | null>(
+    notifications.length > 0
+      ? notifications[notifications.length - 1].id
+      : null,
+  );
+  const [lastDataLength, setLastDataLength] = useState<number>(5);
   const handleResize = () => {
     setWindowWidth(window.innerWidth);
   };
@@ -47,24 +57,45 @@ const NotificationModal = ({
   const fetchNotifications = useCallback(async () => {
     setLoading(true);
     try {
-      const cursorId =
-        notifications.length > 0
-          ? notifications[notifications.length - 1].id
-          : 0;
-      const response = await getMyNotifications({ cursorId, size: 10 });
+      const response = await getMyNotifications({
+        cursorId: lastCursorId,
+        size: 5,
+      });
       setNotifications((prev) => [...prev, ...response.notifications]);
+      if (response.notifications.length > 0) {
+        setLastCursorId(
+          response.notifications[response.notifications.length - 1].id,
+        );
+      }
+      setLastDataLength(response.notifications.length);
+      setCursorId(response.cursorId);
     } catch (error) {
       console.error("Failed to fetch notifications", error);
     } finally {
       setLoading(false);
     }
-  }, [notifications, setNotifications]);
+  }, [setNotifications, lastCursorId]);
 
   useEffect(() => {
-    if (inView && !loading) {
+    if (
+      inView &&
+      !loading &&
+      totalCount > 5 &&
+      cursorId &&
+      lastDataLength === 5
+    ) {
+      console.log(totalCount);
+      console.log(cursorId);
       fetchNotifications();
     }
-  }, [inView, loading, fetchNotifications]);
+  }, [
+    inView,
+    loading,
+    fetchNotifications,
+    totalCount,
+    cursorId,
+    lastDataLength,
+  ]);
 
   const handleDelete = async (id: number) => {
     try {
@@ -124,39 +155,54 @@ const NotificationModal = ({
     <>
       <div
         ref={notificationsRef}
-        className="fixed z-[50] w-full h-screen bg-[#CED8D5] border border-[#CBC9CF] rounded-[10px] shadow-[0px_2px_8px_0px_rgba(120,116,134,0.25)] md:absolute md:w-[368px] md:max-h-[calc(4*152px+64px)]"
+        className="over fixed z-[50] h-screen w-full rounded-[10px] border border-[#CBC9CF] bg-[#CED8D5] shadow-[0px_2px_8px_0px_rgba(120,116,134,0.25)] md:absolute md:max-h-[calc(4*152px+64px)] md:w-[368px]"
         style={{
           top: windowWidth >= 768 ? buttonPosition.top + 10 : 0,
-          left: windowWidth >= 768 ? buttonPosition.left - 170 : buttonPosition.left,
+          left:
+            windowWidth >= 768
+              ? buttonPosition.left - 170
+              : buttonPosition.left,
         }}
       >
-        <div id="notificationModal" className="flex items-center justify-between p-6">
+        <div
+          id="notificationModal"
+          className="flex items-center justify-between p-6"
+        >
           <p className="text-[20px] font-bold leading-normal">
-            {`알림 ${notifications.length}개`}
+            {`알림 ${totalCount}개`}
           </p>
           <button
             type="button"
-            onClick={() => setState(prev => !prev)}
+            onClick={() => setState((prev) => !prev)}
             className="h-10 w-10 bg-[url('/icons/btn_X.svg')] bg-cover"
           ></button>
         </div>
         {notifications.length === 0 ? (
-          <div className="flex flex-col items-center justify-center h-full text-center pb-32">
-            <AlertSvg isClicked={false}/>
-            <p className="text-[20px] font-bold mt-4">새로운 알림이 없습니다.</p>
-            <p className="text-[14px] text-[#a4a1aa]">서비스와 다양한 알림을 이곳에서 모아볼 수 있어요.</p>
+          <div className="flex h-full flex-col items-center justify-center pb-32 text-center">
+            <AlertSvg isClicked={false} />
+            <p className="mt-4 text-[20px] font-bold">
+              새로운 알림이 없습니다.
+            </p>
+            <p className="text-[14px] text-[#a4a1aa]">
+              서비스와 다양한 알림을 이곳에서 모아볼 수 있어요.
+            </p>
           </div>
         ) : (
-          <div className="mt-4 flex h-full flex-col gap-2 overflow-hidden md:h-[calc(2*120px+64px)]">
+          <div
+            className="mt-4 flex h-full flex-col items-center gap-2 px-4 md:h-[calc(4*120px+64px)]"
+            style={{ overflowY: "scroll", scrollbarWidth: "none" }}
+          >
             {notifications.map((notification) => (
               <div
                 key={notification.id}
-                className="min-h-[120px] bg-white px-3 py-4"
+                className="relative min-h-[120px] w-full rounded-lg bg-white px-3 pb-4 pt-2 md:w-[340px]"
               >
                 <div className="flex items-start justify-between">
-                  <div
-                    className={`mt-1 h-[5px] w-[5px] rounded-full ${notification.content.includes("승인") ? "bg-[#0085ff]" : "bg-[#ff472e]"}`}
-                  ></div>
+                  <div className="flex h-3 items-center justify-center">
+                    <div
+                      className={` mt-1  h-[5px] w-[5px]  rounded-full ${notification.content.includes("승인") ? "bg-[#0085ff]" : "bg-[#ff472e]"}`}
+                    />
+                  </div>
                   <button
                     type="button"
                     onClick={() => handleDelete(notification.id)}
@@ -164,19 +210,23 @@ const NotificationModal = ({
                   ></button>
                 </div>
                 <div className="flex flex-col justify-between">
-                  <p
-                    className="mb-1 text-[14px] font-normal leading-[22px] break-words"
-                  >
+                  <p className="mb-1 break-words text-[14px] font-normal leading-[22px]">
                     {renderContent(notification.content)}
                   </p>
-                  <p className="text-[12px] font-normal leading-[16px] text-[#a4a1aa]">
-                    {dateFormat(notification.updatedAt)}
-                  </p>
                 </div>
+                <p className=" absolute bottom-3 left-3 text-[12px] font-normal leading-[16px] text-[#a4a1aa]">
+                  {dateFormat(notification.updatedAt)}
+                </p>
               </div>
             ))}
             <div ref={ref} className="h-[1px]"></div>
-            {loading && <div className="p-4 text-center">로딩 중...</div>}
+            {loading && (
+              <div className="flex h-32 w-full items-center justify-center space-x-2 dark:invert md:h-[calc(4*120px+64px)]">
+                <div className="h-3 w-3 animate-bounce rounded-full bg-nomad-black [animation-delay:-0.3s]"></div>
+                <div className="h-3 w-3 animate-bounce rounded-full bg-nomad-black [animation-delay:-0.15s]"></div>
+                <div className="h-3 w-3 animate-bounce rounded-full bg-nomad-black"></div>
+              </div>
+            )}
           </div>
         )}
       </div>
